ff556e63f5ee68b4607b89f72372a0ee8ad782c6
mikey13975 <mikey13975@gmail.com>
2023-04-30 04:34:36 -0500

    create math functions and vars for user input

---
+++ odinCalc.js
@@ -0,0 +1,28 @@
+function add(a,b) {
+	const sum = a + b;
+	return sum;
+};
+
+function subtract(a,b) {
+	const difference = a - b;
+	return difference;	
+};
+
+function multiply(a,b) {
+	const product = a * b;
+	return product;
+};
+
+function divide(a,b) {
+	const dividend = a / b;
+	return dividend;
+};
+
+//let a = user input first number;
+let a = "";
+
+//let op = user input operator;
+let op = "";
+
+//let b = user input second number;
+let b = "";
