fb91980efefe56558f4f920576460eea0402668d
mikey13975 <mikey13975@gmail.com>
2023-06-16 19:51:05 -0500

    completed calculator

--- odinCalc.js
+++ odinCalc.js
@@ -1,20 +1,22 @@
 function add(a,b) {
-	const sum = a + b;
+	
+	let sum = parseFloat(a) + parseFloat(b);
+	console.log(sum);
 	return sum;
 };
 
 function subtract(a,b) {
-	const difference = a - b;
+	const difference = parseFloat(a) - parseFloat(b);
 	return difference;	
 };
 
 function multiply(a,b) {
-	const product = a * b;
+	const product = parseFloat(a) * parseFloat(b);
 	return product;
 };
 
 function divide(a,b) {
-	const dividend = a / b;
+	const dividend = parseFloat(a) / parseFloat(b);
 	return dividend;
 };
 
@@ -27,26 +29,14 @@ let op;
 let b;
 
 // display.innertext = "0";
-function operate(a,op,b) {
-	if(op === "+") {
-		answer = add(a,b);
-	} else if(op === "-") {
-		answer = subtract(a,b);
-	} else if(op === "*") {
-		answer = multiply(a,b);
-	} else if (op === "/") {
-		answer = divide(a,b);
-	}
-	return answer;
-};
 function btnDisplay() {;
 	const num = document.getElementsByClassName('numBtn');
-	const operate = document.getElementsByClassName('operator');
+	const operator = document.getElementsByClassName('operator');
 	let btnPress = document.querySelectorAll("button");
 	// console.log(btnPress);
 	let display = document.getElementById('display');
-	
-	display.innerText = "0";
+
+	// display.innerText = "0";
 	// console.log(display.textContent);
 	for (i = 0; i <btnPress.length ; i++){
 		btnPress[i].addEventListener('click',function(e){
@@ -54,35 +44,70 @@ function btnDisplay() {;
 			let screen = display.innerText;
 			console.log(a, op, b);
 			
-			if(key.className === "numBtn" && screen === "0"){
+			if(key.className === "numBtn" && a === undefined){
 				display.innerText = key.innerText;
 				a = key.innerText;
 				console.log(a);
-				} else if(screen !== "0" && key.className === "numBtn" && op === undefined && b === undefined ){
+				} else if(a !== undefined && key.className === "numBtn" && op === undefined && b === undefined ){
 					display.innerText += `${key.innerText}`;
 					a += `${key.innerText}`;
 					console.log(a);
 					console.log(typeof(a));
 				// console.log(screen);
 				console.log("you pressed a number!");}
-				else if(key.className === "operator") {
+				else if(key.className === "operator" && a !== undefined && b === undefined ) {
 					op = key.innerText;
 					display.innerText += `${key.innerText}`;
 					console.log("that's an operator");
+				}
+				else if (key.className === "operator" && a !== undefined && b !== undefined) {display.innerText = operate() + `${key.innerText}`;
+					a = operate()
+					b = '';
+					op = key.innerText;
 				}
 				
 				else if( key.className === "numBtn" && a !== undefined && op !== undefined && b === undefined){
 					b = key.innerText;
 					display.innerText += `${key.innerText}`;
-					console.log(a, op, b);
 				}
 
 				else if (key.className === "numBtn" && a !== undefined && op !== undefined ) {
 					b += `${key.innerText}`;
 					display.innerText += `${key.innerText}`;
+					console.log(a, op, b);
 				}
-			})
-		
-	}
-}
+				if (key.className === "calculate")  {display.innerHTML = operate();
+					a = undefined;
+					b = undefined;
+					op = undefined;
+					console.log(operate())
+					;
+					
+					
+			}
+			
+
+			if (key.className === "displayClear") {a = undefined;
+			b = undefined;
+			op = undefined;
+			display.innerText = "0";}
+
+	})}}
+	// return a, b, op;
+	// console.log`${btnDisplay()}`;
+	
 	btnDisplay();
+	function operate(btnDisplay) {
+		let answer;
+		if(op === "+") {
+			answer = add(a,b);
+		} else if(op === "-") {
+			answer = subtract(a,b);
+		} else if(op === "*") {
+			answer = multiply(a,b);
+		} else if (op === "/") {
+			answer = divide(a,b);
+		}
+		return answer;
+	};
+		// console.log(operate(btnDisplay()))
